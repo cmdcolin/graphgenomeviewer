e653b1d1019a03ee267671a952b8d2079777107e
Colin <colin.diesh@gmail.com>
2020-07-23 16:50:16 -0400

    Use react-bootstrap for some basic styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import React, { useEffect, useRef } from 'react'
 import './App.css'
 import igv from 'igv'
 import { Graph } from './Graph'
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import graph from './MT.json'
 
 function App() {
@@ -13,16 +23,33 @@ function App() {
 
   return (
     <div>
-      <div className="header">
-        <h1>graphgenome browser</h1>
-      </div>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">graphgenome browser</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
       <div className="with-sidebar">
         <div id="sidebar" className="sidebar">
-          Test
           <Graph graph={graph} />
         </div>
         <div className="body">
-          Body
           <div
             ref={ref}
             style={{
