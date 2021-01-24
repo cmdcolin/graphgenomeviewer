ebae06792d5fa8df533fb743d926bb8d2be98d40
Colin <colin.diesh@gmail.com>
2021-01-24 10:13:28 -0700

    Recreate create-react-app instance

--- app/src/Header.js
+++ app/src/Header.js
@@ -1,35 +1,38 @@
 import React, { useRef, useState } from 'react'
 import { Button, Form, Modal, Navbar, Nav, NavDropdown } from 'react-bootstrap'
 import { SettingsDialog } from './SettingsDialog'
+import pkg from '../package.json'
 
 function AboutDialog({ onHide }) {
   return (
     <Modal show={true} onHide={onHide}>
       <Modal.Header closeButton>
-        <Modal.Title>graphgenome browser</Modal.Title>
+        <Modal.Title>graphgenome browser {pkg.version}</Modal.Title>
       </Modal.Header>
 
       <Modal.Body>
         <p>
-          This is a small demo of browsing simple graph genomes. The app can read GFA
-          format URLs and local files, all processing is done client side currently.
+          This is a small demo of browsing simple graph genomes. The app can
+          read GFA format URLs and local files, all processing is done client
+          side currently.
         </p>
         <p>
-          Contact <a href="mainto:colin.diesh@gmail.com">Colin Diesh</a>
+          Contact <a href='mainto:colin.diesh@gmail.com'>Colin Diesh</a>
         </p>
         <p>
           Thanks to the BCC2020 pangenome team, and{' '}
-          <a href="https://github.com/rrwick/Bandage">Bandage</a> for inspiration.
+          <a href='https://github.com/rrwick/Bandage'>Bandage</a> for
+          inspiration.
         </p>
         <p>
-          Sample data from GFA-spec repo (MT.gfa), Andrea Guarracino (path example), and
-          gfalint (Shaun Jackman)
+          Sample data from GFA-spec repo (MT.gfa), Andrea Guarracino (path
+          example), and gfalint (Shaun Jackman)
         </p>
         <p>
-          This app attempts to support GFA2 but does not properly render fragments,
-          intricate edge details, or U and O paths
+          This app attempts to support GFA2 but does not properly render
+          fragments, intricate edge details, or U and O paths
         </p>
-        <a href="https://github.com/cmdcolin/graphgenomeviewer">GitHub</a>
+        <a href='https://github.com/cmdcolin/graphgenomeviewer'>GitHub</a>
       </Modal.Body>
     </Modal>
   )
@@ -53,12 +56,12 @@ function OpenURLDialog({ onHide, onData }) {
           <Form.Group>
             <Form.Label>Enter URL</Form.Label>
             <Form.Control
-              type="input"
+              type='input'
               value={value}
-              onChange={event => setValue(event.target.value)}
+              onChange={(event) => setValue(event.target.value)}
             />
           </Form.Group>
-          <Button type="submit">Submit</Button>
+          <Button type='submit'>Submit</Button>
         </Form>
       </Modal.Body>
     </Modal>
@@ -75,7 +78,7 @@ function OpenFileDialog({ onHide, onGraph }) {
 
       <Modal.Body>
         <Form
-          onSubmit={async event => {
+          onSubmit={async (event) => {
             event.preventDefault()
             const data = await ref.current.files[0].text()
             onGraph(data)
@@ -84,9 +87,9 @@ function OpenFileDialog({ onHide, onGraph }) {
         >
           <Form.Group>
             <Form.Label>Open file</Form.Label>
-            <Form.Control ref={ref} type="file" />
+            <Form.Control ref={ref} type='file' />
           </Form.Group>
-          <Button type="submit">Submit</Button>
+          <Button type='submit'>Submit</Button>
         </Form>
       </Modal.Body>
     </Modal>
@@ -100,12 +103,12 @@ export function Header({ onData, settings, onGraph, onExportSVG, onSettings }) {
   const [showOpenFile, setShowOpenFile] = useState()
   return (
     <>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">graphgenome browser</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <NavDropdown title="File" id="basic-nav-dropdown">
+      <Navbar bg='light' expand='lg'>
+        <Navbar.Brand href='#home'>graphgenome browser</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='mr-auto'>
+            <NavDropdown title='File' id='basic-nav-dropdown'>
               <NavDropdown.Item onClick={() => setShowOpenURL(true)}>
                 Open URL
               </NavDropdown.Item>
@@ -116,7 +119,7 @@ export function Header({ onData, settings, onGraph, onExportSVG, onSettings }) {
                 Export SVG
               </NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title="Examples" id="basic-nav-dropdown">
+            <NavDropdown title='Examples' id='basic-nav-dropdown'>
               <NavDropdown.Item onClick={() => onData('MT.gfa')}>
                 MT GFA-spec example
               </NavDropdown.Item>
@@ -126,10 +129,16 @@ export function Header({ onData, settings, onGraph, onExportSVG, onSettings }) {
               <NavDropdown.Item onClick={() => onData('example1.gfa')}>
                 Paths example 2
               </NavDropdown.Item>
-              <NavDropdown.Item onClick={() => onData('big1.gfa')}>Big1</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => onData('ir1.gfa')}>Ir1</NavDropdown.Item>
+              <NavDropdown.Item onClick={() => onData('big1.gfa')}>
+                Big1
+              </NavDropdown.Item>
+              <NavDropdown.Item onClick={() => onData('ir1.gfa')}>
+                Ir1
+              </NavDropdown.Item>
               <NavDropdown.Item
-                onClick={() => onData('test_contig_placement_assembly_graph.gfa')}
+                onClick={() =>
+                  onData('test_contig_placement_assembly_graph.gfa')
+                }
               >
                 Unicycler example
               </NavDropdown.Item>
@@ -169,7 +178,10 @@ export function Header({ onData, settings, onGraph, onExportSVG, onSettings }) {
         <OpenURLDialog onData={onData} onHide={() => setShowOpenURL(false)} />
       ) : null}
       {showOpenFile ? (
-        <OpenFileDialog onGraph={onGraph} onHide={() => setShowOpenFile(false)} />
+        <OpenFileDialog
+          onGraph={onGraph}
+          onHide={() => setShowOpenFile(false)}
+        />
       ) : null}
     </>
   )
