b24f328906835e253fee17ea6df1033cd26d0c8c
Colin <colin.diesh@gmail.com>
2024-06-20 14:58:55 -0400

    Multiple instances check

--- lib/stories/Graph.stories.tsx
+++ lib/stories/Graph.stories.tsx
@@ -20,6 +20,35 @@ function MTGraph() {
   return <GFAGraph url="MT.gfa" />
 }
 
-export { BasicExample, MTGraph }
+function MultipleInstances() {
+  return (
+    <div>
+      <Graph
+        width={500}
+        height={300}
+        graph={{
+          nodes: [
+            { id: 'id1', length: 5000 },
+            { id: 'id2', length: 10000 },
+          ],
+          links: [{ source: 'id1', target: 'id2', id: 'edge1' }],
+        }}
+      />
+      <Graph
+        width={500}
+        height={300}
+        graph={{
+          nodes: [
+            { id: 'id1', length: 5000 },
+            { id: 'id2', length: 10000 },
+          ],
+          links: [{ source: 'id1', target: 'id2', id: 'edge1' }],
+        }}
+      />
+    </div>
+  )
+}
+
+export { BasicExample, MTGraph, MultipleInstances }
 
 export default { title: 'Examples' }
