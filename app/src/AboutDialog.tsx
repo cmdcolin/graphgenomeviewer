33110d25db01649850c10dd173b3730a195c2354
Colin <colin.diesh@gmail.com>
2024-12-22 10:54:35 -0500

    Updates

--- app/src/AboutDialog.tsx
+++ app/src/AboutDialog.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Modal } from 'react-bootstrap'
 
 export default function AboutDialog({ onHide }: { onHide: () => void }) {
--- app/src/FeatureDialog.tsx
+++ app/src/FeatureDialog.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Button, Modal } from 'react-bootstrap'
 
 export default function FeatureDialog({
