56f619f9a1642e60857942a184bf15f47518d990
Colin <colin.diesh@gmail.com>
2024-12-22 10:53:14 -0500

    Update vite template

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './',
-  build: { sourcemap: true },
 })
